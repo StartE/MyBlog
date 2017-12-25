9fd9957a0f4fb61577c027c1aea5afa9559d0855
StartE <yili0618@qq.com>
2017-12-25 22:04:53 +0800

    add abstract

--- src/app/main-console/common/abstract/abstract.component.ts
+++ src/app/main-console/common/abstract/abstract.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-abstract',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbstractComponent implements OnInit {
 
+  @Input() articleTitle:string;
+  @Input() words:number;
+  @Input() content:string;
+  @Input() type:string;
+  cutString: string;
+  showHint:boolean = false;
+  hint1:string = "Show All";
+  hint2:string = "Hide Remaining";
+  hint:string = this.hint1;
   constructor() { }
 
   ngOnInit() {
+    this.generateAbstract();
   }
-
+  generateAbstract(){
+    if(this.content.length < this.words){
+      this.showHint = false;
+      this.cutString = this.content;
+    }else{
+      this.showHint = true;
+      this.cutString = this.content.substring(0,this.words)+'...';
+    }    
+  }
+  toggleBtn(){
+    if (this.hint == this.hint1){
+      this.cutString = this.content;
+      this.hint = this.hint2;
+    }else if(this.hint == this.hint2){
+      this.cutString = this.content.substring(0,this.words)+'...';
+      this.hint = this.hint1;
+    }
+  }
 }
